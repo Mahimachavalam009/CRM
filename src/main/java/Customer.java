a14999e7602e54b4be71bae1cbe624e1e093e0a8
MahimaChavalam <sussybakka69696@gmail.com>
2024-10-27 21:00:34 +0530

    Create Customer.java

---
+++ src/main/java/Customer.java
@@ -0,0 +1,20 @@
+package com.example.crm.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+}
